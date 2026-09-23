960b7beb87d80fdceb8c268b292689a1dcf99626
davidsaju <mgt9w57w2m@privaterelay.appleid.com>
2026-09-23 10:47:08 +0000

    Updated lab 1, 2, 3

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -12,8 +12,11 @@
 */
 
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    return 0; // placeholder
+   int sum=0;
+   for(int i=1;i<=n;i++){
+    sum+=i;
+   }
+    return sum; 
 }
 
 int main(void) {
@@ -21,8 +24,10 @@ int main(void) {
 
     printf("Enter a positive integer n: ");
     scanf("%d", &n);
-
-    // TODO: validate input, call function, and print result
-
+    if(n<1){
+        printf("Error; n must be at least 1. \n");
+    } else {
+        printf("Sum=%d\n", sum_to_n(n));
+    }
     return 0;
 }
--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,8 +12,11 @@
 */
 
 long long factorial(int n) {
-    // TODO: compute factorial iteratively
-    return 1; // placeholder
+    long long result=1;
+    for(int i=1;i<=n;i++){
+        result*=i;
+    }
+    return result;
 }
 
 int main(void) {
@@ -21,8 +24,10 @@ int main(void) {
 
     printf("Enter a non-negative integer n: ");
     scanf("%d", &n);
-
-    // TODO: validate input, call function, print result
-
+    if(n<0){
+        printf ("Error: n must not be negative. \n");
+        return 0;
+    }
+    printf("%lld\n", factorial(n));
     return 0;
 }
